49b4623817f4d936fa700858cf04f6242dad48ad
Tony Austin <t.austin@nhm.ac.uk>
2015-02-17 16:40:01 +0000

    Changes to press release layout

--- core/src/main/java/uk/ac/nhm/nhm_www/core/componentHelpers/PressReleaseHelper.java
+++ core/src/main/java/uk/ac/nhm/nhm_www/core/componentHelpers/PressReleaseHelper.java
@@ -1,6 +1,8 @@
 package uk.ac.nhm.nhm_www.core.componentHelpers;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.text.SimpleDateFormat;
 
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ValueMap;
@@ -65,6 +67,28 @@ public class PressReleaseHelper {
 
 	public Date getPublishDate() {
 		return publishDate;
+	}
+	
+	public String getFormattedPublishDate()
+	{
+		String dateStr;
+		
+		final String[] suffixes =
+				  //    0     1     2     3     4     5     6     7     8     9
+				     { "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th",
+				  //    10    11    12    13    14    15    16    17    18    19
+				       "th", "th", "th", "th", "th", "th", "th", "th", "th", "th",
+				  //    20    21    22    23    24    25    26    27    28    29
+				       "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th",
+				  //    30    31
+				       "th", "st" };
+
+		 Date date = new Date();
+		 SimpleDateFormat formatDayOfMonth  = new SimpleDateFormat("d");
+		 int day = Integer.parseInt(formatDayOfMonth.format(date));
+		 String dayStr = day + suffixes[day];
+		 dateStr = dayStr + new SimpleDateFormat(" MMMM yyyy").format(this.getPublishDate());
+		 return dateStr;
 	}
 
 	public void setPublishDate(Date publishDate) {
