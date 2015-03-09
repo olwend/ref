99c614de5039aafd2f218522017b3396ee147062
alextheys <a.theys@nhm.ac.uk>
2015-03-09 15:38:25 +0000

    alext2: writing the JSON object to the response

--- core/src/main/java/uk/ac/nhm/nhm_www/core/impl/servlets/DynamicAppPathRenderingServlet.java
+++ core/src/main/java/uk/ac/nhm/nhm_www/core/impl/servlets/DynamicAppPathRenderingServlet.java
@@ -12,10 +12,11 @@ import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.commons.json.JSONException;
 import org.apache.sling.commons.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.day.cq.wcm.api.Page;
 
@@ -25,11 +26,11 @@ import uk.ac.nhm.nhm_www.core.services.DynamicAppPageRenderingService;
 @Component(label = "Natural History Museum Dynamaic App Path Rendering Servlet", description = "Natural History Museum Dynamaic App Path Rendering Servlet", metatype = true, immediate = true)
 @Service(value = Servlet.class)
 @Properties({
-		@Property(name = "sling.servlet.paths", value = { "/bin/list/pagination" }, propertyPrivate = true),
+		@Property(name = "sling.servlet.paths", value = { "/bin/list/appintegration" }, propertyPrivate = true),
 		@Property(name = "sling.servlet.extensions", value = {"json"}),
 		@Property(name = "sling.servlet.methods", value = { "GET" }, propertyPrivate = true),
 		@Property(name = "service.description", value = "Return the Path of the required item"),
-		@Property(name = "itemID", value = "home", description = "The default item ID"),
+		@Property(name = "pageId", value = "home", description = "The default item ID"),
 		
 		
 })
@@ -39,6 +40,8 @@ public class DynamicAppPathRenderingServlet extends SlingAllMethodsServlet {
 	 * 
 	 */
 	
+	private static final Logger LOG = LoggerFactory.getLogger(DynamicAppPathRenderingServlet.class);
+	
 	@Reference
 	DynamicAppPageRenderingService pageRenderingService;
 	
@@ -47,9 +50,16 @@ public class DynamicAppPathRenderingServlet extends SlingAllMethodsServlet {
 	
 	@Override
 	protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException, NumberFormatException {
-		String itemID = request.getParameter("itemID");
-		Page page = pageRenderingService.getPage(request, itemID);
-		JSONObject jsonObject = pageRenderingService.getJSON(page);
+		String pageId = request.getParameter("pageId");
+		Page page = pageRenderingService.getPage(request, pageId);
+		try {
+			JSONObject jsonObject = pageRenderingService.getJSON(page);
+			response.setCharacterEncoding("UTF-8");
+			response.getWriter().write(jsonObject.toString());
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			LOG.error("Dynamic app template rendering error JSON Exception ", e);
+		}
 		
 		
 		
