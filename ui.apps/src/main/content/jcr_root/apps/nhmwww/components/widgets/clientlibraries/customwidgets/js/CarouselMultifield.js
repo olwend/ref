b868013f3b3a520167947670b78a367fd07c20e8
alextheys <a.theys@nhm.ac.uk>
2015-04-16 10:56:45 +0100

    alext2: fixing JS

--- ui.apps/src/main/content/jcr_root/apps/nhmwww/components/widgets/clientlibraries/customwidgets/js/CarouselMultifield.js
+++ ui.apps/src/main/content/jcr_root/apps/nhmwww/components/widgets/clientlibraries/customwidgets/js/CarouselMultifield.js
@@ -144,7 +144,7 @@ NHM.CarouselItemForm =  CQ.Ext.extend(CQ.Ext.Panel, {
         this.itemHeading = new CQ.form.RichText({
             cls:"",
             width:"350px",
-            removeSingleParagraphContainer:true; 
+            removeSingleParagraphContainer:true,
              listeners: {
                  change: {
                      scope:this,
