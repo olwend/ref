d5c1048215ce1110d8ee4143b6d16095e83b130c
Ramon Antunez <r.antunez@nhm.ac.uk>
2015-05-05 16:46:08 +0100

    Quick commit - Adding CTA (fixes needed)

--- core/src/main/java/uk/ac/nhm/nhm_www/core/componentHelpers/GrandSummaryHelper.java
+++ core/src/main/java/uk/ac/nhm/nhm_www/core/componentHelpers/GrandSummaryHelper.java
@@ -3,6 +3,8 @@ package uk.ac.nhm.nhm_www.core.componentHelpers;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.ValueMap;
 
+import uk.ac.nhm.nhm_www.core.utils.LinkUtils;
+
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.components.DropTarget;
 import com.day.cq.wcm.foundation.Image;
@@ -18,22 +20,36 @@ public class GrandSummaryHelper {
 	private boolean componentInitialised;
 	private Image mobileimage;
 	private Object componentType;
+	private Boolean hasCTA;
+	private String callToActionTitle;
+	private String callToActionLink;
+	private Boolean callToActionLinkNewWindow;
+	private Boolean hasCTAIcon;
+	private String iconClass;
+	private String sectionOverride;
+	private boolean activated;
 
 	public GrandSummaryHelper(SlingHttpServletRequest request, Page page, ValueMap properties, Image image) {
-		this.componentType = properties.get("component-type", String.class);
-		this.summary = properties.get("summary", String.class);
+		// Title, Link and New Window
 		this.title = properties.get("title", String.class);
+		
+		// Summary
+		this.summary = properties.get("summary", String.class);
 
+		// Component Type
+		this.componentType = properties.get("component-type", String.class);
+
+		// Image Handling
 		this.mobileimage = new Image(request.getResource());
 		this.mobileimage.setIsInUITouchMode(Placeholder.isAuthoringUIModeTouch(request));
 		this.image = image;
 		this.image.setIsInUITouchMode(Placeholder.isAuthoringUIModeTouch(request));
-
 		// drop target css class = dd prefix + name of the drop target in the
 		// edit config
 		this.image.addCssClass(DropTarget.CSS_CLASS_PREFIX + "image");
 		this.image.addCssClass("staticHero");
 		this.image.setSelector(".img"); // use image script
+		
 		if (this.getAltText() != null) {
 			this.image.setAlt(this.getAltText());
 			this.image.setTitle(this.getAltText());
@@ -41,6 +57,32 @@ public class GrandSummaryHelper {
 
 		if (request.getResource().getPath() != null) {
 			divId = request.getResource().getPath();
+		}
+		
+		// Call to Action
+		this.hasCTA = properties.get("addCallToAction", false);
+		if(this.hasCTA){
+			// CTA Title, Link & New Window
+			this.callToActionTitle = properties.get("cta-title", "");
+			this.callToActionLink = properties.get("cta-path", "");
+			if(!this.callToActionTitle.equals("") && !this.callToActionLink.equals("")) {
+				this.callToActionLink= LinkUtils.getFormattedLink(this.callToActionLink);
+			}
+			if (properties.get("callToActionNewWindow") != null) {
+				this.callToActionLinkNewWindow = properties.get("cta-newwindow",false);
+			}
+			// Do we want the icon?
+			this.hasCTAIcon = properties.get("cta-icon", false);
+			
+			// Icon Type
+			this.iconClass = properties.get("calltoaction-type", "");
+			
+			// Section Override
+			this.sectionOverride = properties.get("section-override", "");
+		}
+		
+		if(this.title!=null && this.summary!=null){ 
+			this.activated = true;
 		}
 	}
 
@@ -70,6 +112,10 @@ public class GrandSummaryHelper {
 			this.mobileimage.setTitle(this.getAltText());
 		}
 		return this.mobileimage;
+	}
+
+	public boolean isActivated() {
+		return activated;
 	}
 
 	public void setImage(Image image) {
@@ -122,6 +168,58 @@ public class GrandSummaryHelper {
 
 	public void setDivId(String divId) {
 		this.divId = divId;
+	}
+
+	public String getIconClass() {
+		return iconClass;
+	}
+
+	public void setIconClass(String iconClass) {
+		this.iconClass = iconClass;
+	}
+
+	public Boolean hasCTA() {
+		return hasCTA;
+	}
+
+	public String getCallToActionTitle() {
+		return callToActionTitle;
+	}
+
+	public void setCallToActionTitle(String callToActionTitle) {
+		this.callToActionTitle = callToActionTitle;
+	}
+
+	public String getCallToActionLink() {
+		return callToActionLink;
+	}
+
+	public void setCallToActionLink(String callToActionLink) {
+		this.callToActionLink = callToActionLink;
+	}
+
+	public Boolean getCallToActionLinkNewWindow() {
+		return callToActionLinkNewWindow;
+	}
+
+	public void setCallToActionLinkNewWindow(Boolean callToActionLinkNewWindow) {
+		this.callToActionLinkNewWindow = callToActionLinkNewWindow;
+	}
+
+	public Boolean hasCTAIcon() {
+		return hasCTAIcon;
+	}
+
+	public void setCTAIcon(Boolean hasCTAIcon) {
+		this.hasCTAIcon = hasCTAIcon;
+	}
+
+	public String getSectionOverride() {
+		return sectionOverride;
+	}
+
+	public void setSectionOverride(String sectionOverride) {
+		this.sectionOverride = sectionOverride;
 	}
 
 }
--- ui.apps/src/main/content/jcr_root/apps/nhmwww/components/functional/grand-summary/grand-summary.jsp
+++ ui.apps/src/main/content/jcr_root/apps/nhmwww/components/functional/grand-summary/grand-summary.jsp
@@ -1,24 +1,48 @@
 <%@page session="false"%>
 <%@include file="/apps/nhmwww/components/global.jsp"%>
 <%@page import="uk.ac.nhm.nhm_www.core.componentHelpers.*"%>
+<%@page import="uk.ac.nhm.nhm_www.core.model.SVGImage" %>
 <cq:defineObjects />
 <cq:includeClientLib />
-<%
-	GrandSummaryHelper helper = new GrandSummaryHelper(slingRequest, currentPage, properties);
+<%	GrandSummaryHelper helper = new GrandSummaryHelper(slingRequest, currentPage, properties); %>
+<% 
+	String svgIcon = "";
+	String svgBaseColor = "";
+	if(helper.hasCTAIcon()){
+		CTAButtonHelper ctahelper = new CTAButtonHelper(properties, resource, request, xssAPI, cssClassSection.toLowerCase());
+		SVGImage svg = ctahelper.getSVGImage(); 
+		svgIcon = svg.toHtml(currentDesign.getPath() + "/");
+		svgBaseColor = svg.getBaseColour();
+	} 
 %>
 
-<div class="video-wrapper" >
-	<% helper.getMobileImage().draw(out); %>
-	<% helper.getImage().draw(out); %>
-
-	<div class="caption-outer-wrapper">
-		<div class="caption-inner-wrapper">
-			<div class="caption">
-				<% if (helper.getTitle() != null) { %> <h1><%=helper.getTitle()%></h1> <% } %>
-				
-				<% if (helper.getSummary() != null) { %> <p><%=helper.getSummary()%></p> <% } %>
+<%	if(helper.isActivated()) { %>
+	<div class="video-wrapper" >
+		<% helper.getMobileImage().draw(out); %>
+		<% helper.getImage().draw(out); %>
+	
+		<div class="caption-outer-wrapper">
+			<div class="caption-inner-wrapper">
+				<div class="caption">
+					<% if (helper.getTitle() != null) { %> <h1><%=helper.getTitle()%></h1> <% } %>
+					
+					<% if (helper.getSummary() != null) { %> <p><%=helper.getSummary()%></p> <% } %>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
+	<% if(helper.hasCTA()){ %>
+		<div class="<%= helper.getSectionOverride() %>">
+			<div class="info-tout info-tout__action tickets">
+				<a class="arrow--large burgandy" href="<%= helper.getCallToActionLink()%>" <%=helper.getCallToActionLinkNewWindow()%> data-gtm="CTA">
+					<%=svgIcon %> 
+					<h3 class="paddingTB"><%=helper.getCallToActionTitle() %></h3>
+					<i class="ico svg-ico arrowl" data-svg-src="/etc/designs/nhmwww/img/svg-icons/icon_l_general_arrow_r.svg" data-svg-title="icon__arrow" data-alt="<%= helper.getIconClass() %>" data-stroke-width="4" data-base-color="<%= svgBaseColor %>"></i>
+				</a>
+			</div>
+		</div>
+	<% } %>
+<% } else { %>
+	<img class="cq-title-placeholder cq-block-lg-placeholder" src="/etc/designs/default/0.gif" />
+<% } %>
 <% %>
