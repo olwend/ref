c8e90ec92936381d053e47771fd5094c5287dbf5
Theo Nicolaou <theo@theo-nicolaou.co.uk>
2015-05-20 16:03:50 +0100

    Fix bug which displays linked h3 header incorrectly

--- core/src/main/java/uk/ac/nhm/nhm_www/core/componentHelpers/LinkListHelper.java
+++ core/src/main/java/uk/ac/nhm/nhm_www/core/componentHelpers/LinkListHelper.java
@@ -276,9 +276,21 @@ public class LinkListHelper extends ListHelper {
 		StringBuffer ret = new StringBuffer();
 		
 		if(this.headersList.get(columnNumber) != null || !this.headersList.equals("")){
-			if(this.headersLinksList.get(columnNumber) != null || !this.headersLinksList.equals("")){ ret.append("<a href=\"" + this.headersLinksList.get(columnNumber) + "\">"); }
-				ret.append("<h3 class=\"linklist--column--header\">" + this.headersList.get(columnNumber) + "</h3>");				
-			if(this.headersLinksList.get(columnNumber) != null || !this.headersLinksList.equals("")){ ret.append("</a>"); }
+			
+			ret.append("<h3 class=\"linklist--column--header\">");
+			
+			
+			if(this.headersLinksList.get(columnNumber) != null || !this.headersLinksList.equals("")){ 
+				ret.append("<a href=\"" + this.headersLinksList.get(columnNumber) + "\">"); 
+			}
+			
+			ret.append(this.headersList.get(columnNumber));
+			
+			if(this.headersLinksList.get(columnNumber) != null || !this.headersLinksList.equals("")){ 
+				ret.append("</a>"); 
+			}
+			
+			ret.append( "</h3>"); 
 		}
 		return ret;
 	}
