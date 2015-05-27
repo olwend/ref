cb12150a86a018c1ab99221a551eb1d59c4d0b5b
Ramon Antunez <r.antunez@nhm.ac.uk>
2015-05-27 12:22:20 +0100

    bugfix to move things around <h3><a> header </a> </h>

--- core/src/main/java/uk/ac/nhm/nhm_www/core/componentHelpers/LinkListHelper.java
+++ core/src/main/java/uk/ac/nhm/nhm_www/core/componentHelpers/LinkListHelper.java
@@ -276,9 +276,11 @@ public class LinkListHelper extends ListHelper {
 		StringBuffer ret = new StringBuffer();
 		
 		if(this.headersList.get(columnNumber) != null || !this.headersList.equals("")){
-			if(this.headersLinksList.get(columnNumber) != null && !this.headersLinksList.get(columnNumber).equals("")){ ret.append("<a href=\"" + this.headersLinksList.get(columnNumber) + "\">"); }
-				ret.append("<h3 class=\"linklist--column--header\">" + this.headersList.get(columnNumber) + "</h3>");				
-			if(this.headersLinksList.get(columnNumber) != null && !this.headersLinksList.get(columnNumber).equals("")){ ret.append("</a>"); }
+			ret.append("<h3 class=\"linklist--column--header\">");
+				if(this.headersLinksList.get(columnNumber) != null && !this.headersLinksList.get(columnNumber).equals("")){ ret.append("<a href=\"" + this.headersLinksList.get(columnNumber) + "\">"); }
+					ret.append(this.headersList.get(columnNumber));				
+				if(this.headersLinksList.get(columnNumber) != null && !this.headersLinksList.get(columnNumber).equals("")){ ret.append("</a>"); }
+			ret.append("</h3>");
 		}
 		return ret;
 	}
