184d2634c211d5112f3b1037f84ce0c5250b572f
Ramon Antunez <r.antunez@nhm.ac.uk>
2015-06-10 12:31:14 +0100

    Lucy's requirement: Fix Names Highlighting / Capitalizing regardless of if the name is using the first initial or all the initials of the Author/Editor name, and regardless if it is all Uppercase, Lowercase,...

--- core/src/main/java/uk/ac/nhm/nhm_www/core/model/science/BookChapter.java
+++ core/src/main/java/uk/ac/nhm/nhm_www/core/model/science/BookChapter.java
@@ -1,11 +1,20 @@
 package uk.ac.nhm.nhm_www.core.model.science;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.nhm.nhm_www.core.componentHelpers.ScientistProfileHelper;
 
 public class BookChapter extends Publication{
+	private static final Logger LOG = LoggerFactory.getLogger(BookChapter.class);
+	
 	private List<String> editors;
 	private String bookTitle;
 
@@ -54,19 +63,43 @@ public class BookChapter extends Publication{
 	}
 
 	@Override
-	public String getHTMLContent(final String currentAuthor, final boolean isFavourite) {
+	public String getHTMLContent(final String author, final boolean isFavourite) {
 		// Butler, R. J. and Barrett, P. M. (2013) Global Cambrian trilobite palaeobiogeography assessed using parsimony analysis of endemicity. In: D.A.T Harper and T Servais (eds) Early Palaeozoic Biogeography and Palaeogeography, pp. 273-296. Geological Society, London.
 		// Authors (publication year) Title. Editors, Book Title, startPage - endPage. Publisher. Place.
 		final List<String> authors = this.getAuthors();
-		String authorsString;
-		if (authors.size() > 5 && isFavourite) {
-			authorsString = StringUtils.join(authors.toArray(new String[authors.size()]), ", ", 0, 5) + ", et al";
-		} else {
-			authorsString = StringUtils.join(authors.toArray(new String[authors.size()]), ", ");
+		String authorsString = StringUtils.EMPTY;
+		
+		// First we normalize the author's name e.g: 
+		// Ouvrard D N M  || OUVRARD DNM >> will become Ouvrard DNM
+		String currentAuthor = normalizeName(author, false);
+		String firstInitial = normalizeName(currentAuthor, true);
+		
+		Iterator<String> authorsIt = authors.iterator();
+		List<String> processedAuthors = new ArrayList<String>();
+		
+		while( authorsIt.hasNext() ){
+			String authorName = authorsIt.next().toString();
+			processedAuthors.add(normalizeName(authorName, false));
 		}
-		authorsString = authorsString.replaceAll(currentAuthor, "<b>" + currentAuthor + "</b>");
+		
+		if (processedAuthors.size() > 5 && isFavourite) {
+			authorsString = StringUtils.join(processedAuthors.toArray(new String[processedAuthors.size()]), ", ", 0, 5) + ", et al";
+		} else {
+			authorsString = StringUtils.join(processedAuthors.toArray(new String[processedAuthors.size()]), ", ");
+		}
+		
+//		LOG.error("This is the list of authors parsed: " + authorsString);
+//		LOG.error("Current Author: " + currentAuthor);
+//		LOG.error("First Initial Author: " + firstInitial);
+		if (authorsString.contains(currentAuthor)) {
+			authorsString = authorsString.replaceAll(currentAuthor, "<b>" + currentAuthor + "</b>");
+		} else if (authorsString.contains(firstInitial)) {
+			authorsString = authorsString.replaceAll(firstInitial, "<b>" + currentAuthor + "</b>");
+		}
+//		LOG.error("After being replaced: " + authorsString);
 		
 		final StringBuffer stringBuffer = new StringBuffer();
+		stringBuffer.append(">>>>>>>>This is a BookChapter<<<<<<<< ");
 		stringBuffer.append(authorsString);
 		
 		stringBuffer.append(" (");
@@ -80,6 +113,12 @@ public class BookChapter extends Publication{
 		if (editors != null) {
 			String editorsString = StringUtils.join(editors.toArray(new String[editors.size()]), ", ");
 			editorsString = editorsString.replaceAll(currentAuthor, "<b>" + currentAuthor + "</b>");
+			
+			if (editorsString.contains(currentAuthor)) {
+				editorsString = editorsString.replaceAll(currentAuthor, "<b>" + currentAuthor + "</b>");
+			} else if (authorsString.contains(firstInitial)) {
+				editorsString = editorsString.replaceAll(firstInitial, "<b>" + currentAuthor + "</b>");
+			}
 			
 			stringBuffer.append(", ");
 			stringBuffer.append(editorsString);
@@ -109,6 +148,8 @@ public class BookChapter extends Publication{
 			stringBuffer.append(this.place);
 			stringBuffer.append(".");
 		}
+		
+//		LOG.error("#### The final result for the publication is: " + stringBuffer + "####"); 
 		
 		return stringBuffer.toString();
 	}
