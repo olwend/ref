f0c14eb28fb76b527d35882f30b4c564fb321c55
Ramon Antunez <r.antunez@nhm.ac.uk>
2015-06-30 12:13:02 +0100

    Bugfix to fill data from URL on pageload

--- ui.apps/src/main/content/jcr_root/etc/clientlibs/nhmwww/sciencelist/js/sciencelist.js
+++ ui.apps/src/main/content/jcr_root/etc/clientlibs/nhmwww/sciencelist/js/sciencelist.js
@@ -9,9 +9,6 @@ var departmentDivision = "";
 var $collectionGroupSelected;;
 var collectionsGroup = "";
 
-var loadDepartmentFromURL = false;
-var loadCollectionsFromURL = false;
-
 var ignoreURL = false;
 
 $.extend({
@@ -181,39 +178,46 @@ function saveSearchTerms() {
 		aux = $.getUrlVar('name');
 		if (!(typeof aux === 'undefined' || aux === null || aux === '')) {
 			name = aux;
+			var $name = document.getElementById("firstNameInput");
+			$name.value = decodeURIComponent(name);
 		}
 		aux = $.getUrlVar('surname');
 		if (!(typeof aux === 'undefined' || aux === null || aux === '')) {
 			surname = aux;
+			var $surname = document.getElementById("surnameInput");
+			$surname.value = decodeURIComponent(surname);
 		}
 		aux = $.getUrlVar('specialism');
 		if (!(typeof aux === 'undefined' || aux === null || aux === '')) {
 			keywords = aux;
+			var $keywords = document.getElementById("keywordsInput");
+			$keywords.value = decodeURIComponent(keywords);
 		}
 		aux = $.getUrlVar('division');
 		if (!(typeof aux === 'undefined' || aux === null || aux === '')) {
-			loadDepartmentFromURL = true;
 			departmentDivision = aux;
 		} else {
 			aux = $.getUrlVar('department');
 			if (!(typeof aux === 'undefined' || aux === null || aux === '')) {
-				loadDepartmentFromURL = true;
 				departmentDivision = aux;
+				var $division = document.getElementById("division");
+				$division.value = decodeURIComponent(departmentDivision);
+				$elementSelected = $("#division option:selected");
 			}
 		}
 //		aux = $.getUrlVar('collection');
 //		if (!(typeof aux === 'undefined' || aux === null || aux === '')) {
-//			loadCollectionsFromURL = true;
 //			collectionsGroup = aux;
 //		} else {
 //			aux = $.getUrlVar('group');
 //			if (!(typeof aux === 'undefined' || aux === null || aux === '')) {
-//				loadCollectionsFromURL = true;
 //				collectionsGroup = aux;
+//				var $collection = document.getElementById("collection");
+//				$collection.value = decodeURIComponent(collectionsGroup);
+//				$collectionGroupSelected = $("#collection option:selected");
 //			}
 //		}
 	}
-	
 	ignoreURL = true;
 }
 
@@ -222,12 +226,7 @@ function searchFunc(maxResults) {
 	var nodes = $("#peopleList").children().children();
 
 	nodes.css("display", "none");
-	
-	if(!ignoreURL) {		
-		var $name = document.getElementById("firstNameInput");
-		$name.value = decodeURIComponent(name);
-	}
-	
+
 	if (name.length != 0) {
 		var lowercase = name.toLowerCase();
 		nodes = nodes.filter(function(){
@@ -241,11 +240,6 @@ function searchFunc(maxResults) {
 			
 			return false;
 		});
-	}
-	
-	if(!ignoreURL) {		
-		var $surname = document.getElementById("surnameInput");
-		$surname.value = decodeURIComponent(surname);
 	}
 
 	if (surname.length != 0) {
@@ -261,11 +255,6 @@ function searchFunc(maxResults) {
 			
 			return false;
 		});
-	}
-	
-	if(!ignoreURL) {		
-		var $keywords = document.getElementById("keywordsInput");
-		$keywords.value = decodeURIComponent(keywords);
 	}
 	
 	if (keywords.length != 0) {
@@ -285,12 +274,6 @@ function searchFunc(maxResults) {
 	        return false;
 		});
 	}
-	
-	if(loadDepartmentFromURL && !ignoreURL) {		
-		var $division = document.getElementById("division");
-		$division.value = decodeURIComponent(departmentDivision);
-		$elementSelected = $("#division option:selected");
-	}
 
 	if (departmentDivision != "All") {
 		if ($elementSelected.hasClass("department")) {
@@ -302,12 +285,6 @@ function searchFunc(maxResults) {
 			var division = $elementSelected.data("division");
 			nodes = nodes.filter("[division=" + '"' + division + '"' + "][department=" + '"' + department + '"' + "]");	
 		}
-	}
-
-	if(loadCollectionsFromURL && !ignoreURL) {		
-		var $collection = document.getElementById("collection");
-		$collection.value = decodeURIComponent(collectionsGroup);
-		$collectionGroupSelected = $("#collection option:selected");
 	}
 	
 	if (collectionsGroup != "All") {
