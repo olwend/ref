380954c849920c83998d6ca72c5e375e74356a6c
Theo Nicolaou <theo@theo-nicolaou.co.uk>
2015-09-11 10:51:47 +0100

    Update to anchor scroll JS for LichenID app

--- ui.apps/src/main/content/jcr_root/etc/clientlibs/nhmwww/main/js/nhm-legacy-lichenid.js
+++ ui.apps/src/main/content/jcr_root/etc/clientlibs/nhmwww/main/js/nhm-legacy-lichenid.js
@@ -1,10 +1,15 @@
+/*
+function to fix the position of the page when you click on a link which scroll to a different #achor, before the sticky nav was added the #anchor would scroll to the top of the page but now the sticky nav covers it so have used offset to position it below the nav. chrome and IE gets a different  offset to FF so the #anchor is positioned a little further down the page.
+this only happens when you click a link from http://www.nhm.ac.uk/nature-online/life/plants-fungi/lichen-id-guide/help.dsml to www.nhm.ac.uk/nature-online/life/plants-fungi/lichen-id-guide/glossary.dsml
+*/ 
+
 function anchorTagPosition() {
 
     if (window.location.hash) {
         var anchorName = window.location.hash.replace("#" , "");
-        var name = $("[name='" + anchorName + "']");
-        console.log(name);
-        console.log(name.offset());
+        //var name = $("[name='" + anchorName + "']");
+        //console.log(name);
+        //console.log(name.offset());
         $('html, body').animate({scrollTop: $("[name='" + anchorName + "']").offset().top -170});
     } else {
         return false;
