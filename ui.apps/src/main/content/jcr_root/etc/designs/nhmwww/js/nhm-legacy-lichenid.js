acf4dc87c81cf68e5c8a66c5274aee80c02e44fc
Theo Nicolaou <theo@theo-nicolaou.co.uk>
2015-10-07 10:22:22 +0100

    Remove commented code from LichenID JS file

--- ui.apps/src/main/content/jcr_root/etc/designs/nhmwww/js/nhm-legacy-lichenid.js
+++ ui.apps/src/main/content/jcr_root/etc/designs/nhmwww/js/nhm-legacy-lichenid.js
@@ -7,9 +7,6 @@ function anchorTagPosition() {
 
     if (window.location.hash) {
         var anchorName = window.location.hash.replace("#" , "");
-        //var name = $("[name='" + anchorName + "']");
-        //console.log(name);
-        //console.log(name.offset());
         $('html, body').animate({scrollTop: $("[name='" + anchorName + "']").offset().top -170});
     } else {
         return false;
