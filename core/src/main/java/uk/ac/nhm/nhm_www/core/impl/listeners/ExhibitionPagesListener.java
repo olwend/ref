6a44d0ccaadcfb8b89b6e3a93320542509da1bd1
Juan <juanm@ensemble.com>
2016-04-22 17:51:12 +0100

    NHMEC-9: Added Exhibition Listener

---
+++ core/src/main/java/uk/ac/nhm/nhm_www/core/impl/listeners/ExhibitionPagesListener.java
@@ -0,0 +1,80 @@
+package uk.ac.nhm.nhm_www.core.impl.listeners;
+
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.observation.Event;
+import javax.jcr.observation.EventIterator;
+import javax.jcr.observation.EventListener;
+import javax.jcr.observation.ObservationManager;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.jcr.api.SlingRepository;
+import org.json.JSONException;
+import org.osgi.service.component.ComponentContext;
+
+import uk.ac.nhm.nhm_www.core.utils.EventPagesUtils;
+
+@Component(immediate = true, metatype = false)
+@Service
+public class ExhibitionPagesListener implements EventListener {
+	private static final String EVENTS_PATH = "content/nhmwww/en/home/events";
+	private static final String EXHIBITIONS_PATH = "content/nhmwww/en/home/visit/exhibitions-and-atractions";
+	private Session session;
+	private ObservationManager exhibitionsObservationManager;
+	
+	@Reference
+	private SlingRepository repository;
+	@Reference
+	private ResourceResolverFactory resolverFactory;
+	
+	//Logic to define the Exhibitions Custom Event Handler
+	protected void activate(ComponentContext ctx) {
+		try {
+			//TODO Search for the newest one
+			session = repository.loginAdministrative(null);
+			exhibitionsObservationManager = session.getWorkspace().getObservationManager();
+			exhibitionsObservationManager.addEventListener(this, Event.NODE_ADDED | Event.NODE_REMOVED | Event.PROPERTY_ADDED | Event.PROPERTY_CHANGED | Event.PROPERTY_REMOVED, "/"+ EXHIBITIONS_PATH, true, null, null, false);
+			
+			//TODO REMOVE THIS LOGGING
+			System.out.println("Observing property changes to Exhibition nodes");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected void deactivate(ComponentContext componentContext) throws RepositoryException {
+		
+		if (exhibitionsObservationManager != null) {
+			exhibitionsObservationManager.removeEventListener(this);
+		}
+		
+		if (session != null) {
+			session.logout();
+			session = null;
+		}
+	}
+	
+	//Triggers the onEvent Function
+	@Override
+	public void onEvent(EventIterator events) {		
+		try {
+			new EventPagesUtils().getEventsDetails(session, EVENTS_PATH, EXHIBITIONS_PATH);
+		} catch (PathNotFoundException e) {
+			System.out.println(e);
+			e.printStackTrace();
+		} catch (RepositoryException e) {
+			System.out.println(e);
+			e.printStackTrace();
+		} catch (JSONException e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+	}
+	
+
+}
