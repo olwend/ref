53f0c0cd5206ad4bec13dbeef6a95ac49982f17d
Jose <jmanuelbr@gmail.com>
2016-07-28 16:43:06 +0100

    Implements When user navigates 'back' to search, results aren't displayed

--- ui.apps/src/main/content/jcr_root/etc/clientlibs/nhmwww/eventcalendarcomponent/js/calendar.js
+++ ui.apps/src/main/content/jcr_root/etc/clientlibs/nhmwww/eventcalendarcomponent/js/calendar.js
@@ -1,4 +1,8 @@
+var searchHistoryArray = [];
+var ignoreHistoryChange = false;
+
 var NHMCalendar = new function () {
+    var searchNumber = 0;
     var CONST = {
         DATE_BUTTON_SELECTED_CLASS: 'date--button--selected',
         DATE_BUTTON_CLASS: 'small-12 medium-12 large-12 columns date--button',
@@ -21,12 +25,12 @@ var NHMCalendar = new function () {
     //Do Search if key pressed is enter
     var getKey = function (event) {
         if (event.which == 13 || event.keyCode == 13) {
-            doSearch();
+            doSearch(true);
         }
     };
 
     //Function to reset the calendar
-    var resetFilters = function () {
+    var resetFilters = function (saveSearch) {
         //Reset the keyword
         inputs.keywordsInput.value = "";
 
@@ -43,7 +47,8 @@ var NHMCalendar = new function () {
         $(CONST.DATE_TO).datepicker(CONST.OPTION, CONST.MIN_DATE, 0);
 
         //Call the search function
-        doSearch();
+//        var save = (typeof )
+        doSearch(saveSearch);
     };
 
     //Function to set the button selected
@@ -65,7 +70,7 @@ var NHMCalendar = new function () {
             $(CONST.DATE_TO).datepicker(CONST.SET_DATE, null);
         }
 
-        doSearch();
+        doSearch(true);
     };
 
     //Function to set the datepicker values from the buttons
@@ -92,7 +97,7 @@ var NHMCalendar = new function () {
 
         $(CONST.DATE_TO).datepicker(CONST.SET_DATE, toDate);
     };
-    
+
     this.init = function () {
         inputs = {
             keywordsInput: document.getElementById("keywordsInput"),
@@ -113,16 +118,16 @@ var NHMCalendar = new function () {
             getKey(e);
         }, false);
         inputs.searchButton[0].addEventListener(CONST.CLICK, function () {
-            doSearch();
+            doSearch(true);
         }, false);
         inputs.resetButton[0].addEventListener(CONST.CLICK, function () {
-            resetFilters();
+            resetFilters(true);
         }, false);
         inputs.filterOne.addEventListener(CONST.CHANGE, function () {
-            doSearch();
+            doSearch(true);
         }, false);
         inputs.filterTwo.addEventListener(CONST.CHANGE, function () {
-            doSearch();
+            doSearch(true);
         }, false);
 
         for (var i = 0; i < inputs.dateButtons.length; i++) {
@@ -132,8 +137,52 @@ var NHMCalendar = new function () {
         }
     };
     
+    var setSearchParameters = function(parameters) {
+        // Clean selection of the date shortcut buttons
+        for (var i = 0; i < inputs.dateButtons.length; i++) {
+            inputs.dateButtons[i].className = CONST.DATE_BUTTON_CLASS;
+        }
+        inputs.keywordsInput.value = parameters.keywordsInput;
+        inputs.filterOne.value = parameters.filterOne;
+        inputs.filterTwo.value = parameters.filterTwo;
+        inputs.dateFrom.value = parameters.dateFrom;
+        inputs.dateTo.value = parameters.dateTo;
+        
+        if (parameters.dateButtons.length > 0) {
+            $('#' + parameters.dateButtons)[0].className += " " + CONST.DATE_BUTTON_SELECTED_CLASS;
+        }
+    };
+    
+    var getSelectedDateButton = function() {
+        var selected = "";
+        if ($('#' + CONST.TODAY)[0].classList.contains(CONST.DATE_BUTTON_SELECTED_CLASS)) {
+            selected = CONST.TODAY;
+        }
+        else if ($('#' + CONST.SEVEN_DAYS)[0].classList.contains(CONST.DATE_BUTTON_SELECTED_CLASS)) {
+            selected = CONST.SEVEN_DAYS;
+        }
+        else if ($('#' + CONST.THIRTY_DAYS)[0].classList.contains(CONST.DATE_BUTTON_SELECTED_CLASS)) {
+            selected = CONST.THIRTY_DAYS;
+        }
+        return selected;
+    }
+    
     //Function to trigger the search
-    var doSearch = function () {
+    var doSearch = function (saveSearchHistory) {
+        if (saveSearchHistory) {
+            ignoreHistoryChange = true;
+            window.location = "#search_" + searchNumber;
+            searchHistoryArray.push({
+                keywordsInput: inputs.keywordsInput.value,
+                dateButtons: getSelectedDateButton(),
+                filterOne: inputs.filterOne.value,
+                filterTwo: inputs.filterTwo.value,
+                dateFrom: inputs.dateFrom.value,
+                dateTo: inputs.dateTo.value  
+            });
+            searchNumber++;
+        }
+        
         if (inputs.keywordsInput.value || inputs.filterOne.value != CONST.NONE || inputs.filterTwo.value != CONST.NONE || inputs.dateFrom.value || inputs.dateTo.value) {
             NHMSearchQuery.displaySearchEvents(inputs.keywordsInput.value, inputs.filterOne.value, inputs.filterTwo.value, inputs.dateFrom.value, inputs.dateTo.value);
         } else {
@@ -142,8 +191,29 @@ var NHMCalendar = new function () {
     };
     
     this.doSearch = doSearch;
-};
+    
+    $(document).ready(function () {
+        NHMCalendar.init();
+    });
 
-$(document).ready(function () {
-    NHMCalendar.init();
-});
+    window.addEventListener('popstate', function(event) {
+        if (ignoreHistoryChange) { // User performed a search 
+            ignoreHistoryChange = false;
+        }
+        else { // User clicked 'Back' or 'Forward' button
+            if(window.location.href.indexOf("#search") > 0) {
+                var searchIndex = parseInt(window.location.href.split('#')[1].replace("search_",""));
+                if (searchIndex < searchHistoryArray.length) {
+                    setSearchParameters(searchHistoryArray[searchIndex]);
+                    doSearch(false);
+                }
+                else { // User tried to go to an url like http://<calendar-url>#searchX in which the searchX parameters don't exist.
+                    window.location = "";
+                }
+            }
+            else { // Show today's events. 
+                resetFilters(false);
+            }
+        }
+    }, false);
+};
