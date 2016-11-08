9583a3c0d30e8d6522784c898dee7ca6652b26c4
Alistair Paterson <a.paterson@nhm.ac.uk>
2016-11-08 10:29:51 +0000

    WR-935: Moved Tor deep linking component to branch off master to allow deployment before Events Calendar

---
+++ core/src/main/java/uk/ac/nhm/nhm_www/core/componentHelpers/TorIframeHelper.java
@@ -0,0 +1,47 @@
+package uk.ac.nhm.nhm_www.core.componentHelpers;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.sling.api.resource.ValueMap;
+
+public class TorIframeHelper {
+
+	protected ValueMap properties;
+	protected HttpServletRequest request;
+	
+	public String iframeCode;
+	
+	public TorIframeHelper(ValueMap properties, HttpServletRequest request) {
+		this.properties = properties;
+		this.request = request;
+		
+		String eventConfigParam = request.getParameter("eventconfig");
+		
+		if(eventConfigParam != null && !eventConfigParam.equals("")) {
+			Pattern pattern = Pattern.compile("^\\d+?");
+	        Matcher matcher = pattern.matcher(eventConfigParam);
+	        
+	        if(matcher.find()) {
+	        	this.iframeCode = 
+	        			"<iframe onload=\"javascript:window.parent.parent.scrollTo(0,0)\" frameborder=\"0\" height=\"1500px\" src=\"https://www.maximweb.net/NHMResponsive_UI/day?eventconfig=" + eventConfigParam + "\" width=\"100%\"> </iframe>";
+	        } else {
+	        	this.iframeCode = 
+	        			"<iframe onload=\"javascript:window.parent.parent.scrollTo(0,0)\" frameborder=\"0\" height=\"1500px\" src=\"https://www.maximweb.net/NHMResponsive_UI/EventsList\" width=\"100%\"> </iframe>";
+	        }
+		} else {
+        	this.iframeCode = 
+        			"<iframe onload=\"javascript:window.parent.parent.scrollTo(0,0)\" frameborder=\"0\" height=\"1500px\" src=\"https://www.maximweb.net/NHMResponsive_UI/EventsList\" width=\"100%\"> </iframe>";
+        }
+	}
+
+	public String getIframeCode() {
+		return iframeCode;
+	}
+
+	public void setIframeCode(String iframeCode) {
+		this.iframeCode = iframeCode;
+	}
+}
