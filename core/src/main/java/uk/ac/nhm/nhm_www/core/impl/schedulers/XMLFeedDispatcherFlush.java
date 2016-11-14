d6c8b0ab815a237222e323338354c7a964cfc353
Alistair Paterson <a.paterson@nhm.ac.uk>
2016-11-14 16:48:31 +0000

    WR-948: Added component and service annotations

--- core/src/main/java/uk/ac/nhm/nhm_www/core/impl/schedulers/XMLFeedDispatcherFlush.java
+++ core/src/main/java/uk/ac/nhm/nhm_www/core/impl/schedulers/XMLFeedDispatcherFlush.java
@@ -1,5 +1,25 @@
 package uk.ac.nhm.nhm_www.core.impl.schedulers;
 
-public class XMLFeedDispatcherFlush {
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component
+@Service
+@Properties({
+	@Property(name="scheduler.expression", value="TODO - Add cron scheduler"),
+})
+public class XMLFeedDispatcherFlush implements Runnable {
+
+	protected final Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
