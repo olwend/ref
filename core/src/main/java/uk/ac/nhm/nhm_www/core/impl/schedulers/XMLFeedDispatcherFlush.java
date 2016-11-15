8c652cae1e17d0012b08ea02a6495a044a8acf30
Alistair Paterson <a.paterson@nhm.ac.uk>
2016-11-15 13:00:34 +0000

    WR-948: Added service to clear cache Added service to rebuild cache - commented out for now

--- core/src/main/java/uk/ac/nhm/nhm_www/core/impl/schedulers/XMLFeedDispatcherFlush.java
+++ core/src/main/java/uk/ac/nhm/nhm_www/core/impl/schedulers/XMLFeedDispatcherFlush.java
@@ -1,5 +1,8 @@
 package uk.ac.nhm.nhm_www.core.impl.schedulers;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.StringRequestEntity;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
@@ -10,16 +13,44 @@ import org.slf4j.LoggerFactory;
 @Component
 @Service
 @Properties({
-	@Property(name="scheduler.expression", value="TODO - Add cron scheduler"),
+	//schedule the service to run every 300s
+	@Property(name="scheduler.period", value="300"),
+	@Property(name="scheduler.expression", value="*/5 * * * ?")
 })
 public class XMLFeedDispatcherFlush implements Runnable {
 
+	private final String xmlFeedPath = "/content/nhmwww/visitorfeed.xml";
+	private final String dispatcherFlushUrl = "/dispatcher/invalidate.cache";
+	
 	protected final Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		try { 
+            //retrieve the request parameters
+            String handle = xmlFeedPath;
+            String page = xmlFeedPath;
+ 
+            //TODO - Need to create a session to get server path from
+            String server = "aem-pblsh1-stg";
+            String port = "5434";
+ 
+            HttpClient client = new HttpClient();
+ 
+            PostMethod post = new PostMethod("http://" + server + ":" + port + dispatcherFlushUrl);
+            post.setRequestHeader("CQ-Action", "Activate");
+            post.setRequestHeader("CQ-Handle", handle);
+
+            //StringRequestEntity body = new StringRequestEntity(page, null, null);
+            //post.setRequestEntity(body);
+            //post.setRequestHeader("Content-length", String.valueOf(body.getContentLength()));
+            client.executeMethod(post);
+            post.releaseConnection();
+            //log the results
+            log.info("Dispatcher flush for page: " + post.getPath() + ", result: " + post.getResponseBodyAsString());
+        } catch(Exception e){
+            log.error("Flushcache servlet exception: " + e.getMessage());
+        }
 	}
 
 }
