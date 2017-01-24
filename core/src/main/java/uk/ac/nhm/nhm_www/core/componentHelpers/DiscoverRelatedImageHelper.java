ac6aa179c5ce7b2caf04c3aa36f23a07ccde61fa
Alistair Paterson <a.paterson@nhm.ac.uk>
2017-01-24 10:03:40 +0000

    WR-993: Clean up of JSPs

--- core/src/main/java/uk/ac/nhm/nhm_www/core/componentHelpers/DiscoverRelatedImageHelper.java
+++ core/src/main/java/uk/ac/nhm/nhm_www/core/componentHelpers/DiscoverRelatedImageHelper.java
@@ -2,7 +2,6 @@ package uk.ac.nhm.nhm_www.core.componentHelpers;
 
 import javax.jcr.AccessDeniedException;
 import javax.jcr.ItemNotFoundException;
-import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,12 +11,12 @@ import org.apache.sling.api.resource.ValueMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import uk.ac.nhm.nhm_www.core.componentHelpers.Foundation5ImageHelper.ImageInterchangeSize;
+import uk.ac.nhm.nhm_www.core.utils.LinkUtils;
+
 import com.adobe.granite.xss.XSSAPI;
 import com.day.cq.commons.ImageResource;
 import com.day.cq.dam.api.Asset;
-
-import uk.ac.nhm.nhm_www.core.utils.LinkUtils;
-import uk.ac.nhm.nhm_www.core.utils.NodeUtils;
 
 public class DiscoverRelatedImageHelper {
 	private String path;
@@ -28,30 +27,24 @@ public class DiscoverRelatedImageHelper {
 	private String caption;
 	private String imageLinkURL;
 	private String mobileImagePath;
-	private Boolean addMarginBottom; // WR-890
 	private boolean newwindow;
 	private Boolean activated;
-	private Resource rs;
-	private Node parentNode;
-	private Asset asset;
 	private ResourceResolver resourceResolver;
+	private Resource rs;
+	private Asset asset;
 	public enum ImageInterchangeSize {DEFAULT, SMALL, MEDIUM, LARGE, RETINA, SMALL_RETINA}
-	
+
 	protected static final Logger logger = LoggerFactory.getLogger(DiscoverRelatedImageHelper.class);
 
 	public DiscoverRelatedImageHelper(ValueMap properties, Resource resource, ResourceResolver resourceResolver, HttpServletRequest request, XSSAPI xssAPI) {
-		
+
 		this.activated = false;
-		
-		this.addMarginBottom = false; // WR-890
-		//this.rs = resource;
+
 		this.resourceResolver = resourceResolver;
-		// WR-890 - set the addMarginBottom Boolean
-		this.addMarginBottom = properties.get("addMarginBottom",false);
-		
+
 		String fileReference = properties.get("fileReference", "");
 		String mobileFileReference = properties.get("mobileFileReference", "");
-		
+
 		logger.error("mobile File Rdeference: " + mobileFileReference);
 		this.originalImagePath = fileReference;
 		this.mobileImagePath = mobileFileReference;
@@ -59,7 +52,7 @@ public class DiscoverRelatedImageHelper {
 			ImageResource image = new ImageResource(resource);
 			String tempPath = request.getContextPath() + resource.getPath();
 			String tempAlt = xssAPI.encodeForHTMLAttr(properties.get("alt", image.getTitle()));
-			
+
 			// Handle extensions on both fileReference and file type images
 			String tempExtension = "jpg";
 			String tempSuffix = "";
@@ -75,12 +68,7 @@ public class DiscoverRelatedImageHelper {
 					tempExtension = mimeType.substring(mimeType.lastIndexOf("/") + 1);
 				}
 			}
-			
-			/*if (mobileFileReference.length() != 0) {
-				tempExtension = mobileFileReference.substring(fileReference.lastIndexOf(".") + 1);
-				tempSuffix = image.getSuffix();
-				tempSuffix = tempSuffix.substring(0, tempSuffix.indexOf('.') + 1) + tempExtension;
-			} */
+
 			tempExtension = xssAPI.encodeForHTMLAttr(tempExtension);
 			this.caption = properties.get("caption","");
 			this.imageLinkURL = properties.get("image-path","");
@@ -97,11 +85,42 @@ public class DiscoverRelatedImageHelper {
 			this.activated = true;
 		}
 	}
-	
-	private int getImageHeight() {
-		return Integer.parseInt(this.getAsset().getMetadataValue("tiff:imageLength"));
+
+	public String getPath(ImageInterchangeSize imageSize) throws AccessDeniedException, ItemNotFoundException, RepositoryException {
+		switch (imageSize) {
+
+		case DEFAULT:
+			return this.getOriginalImagePath();
+
+		case SMALL:
+			if(this.getMobileImagePath() != null && !this.getMobileImagePath().equals("")){
+				return this.getMobileImagePath();
+			}	
+			if (this.getImageWidth() > 768) {
+				return this.getPath() + ".img.320.medium." + this.getExtension() + this.getSuffix();
+			}
+			else {
+				return this.getOriginalImagePath();
+			}
+
+		case MEDIUM:
+			if (this.getImageWidth() > 1024) {
+				return this.getPath() + ".img.480.high." + this.getExtension() + this.getSuffix();
+			}
+			else {
+				return this.getOriginalImagePath();
+			}
+
+		case SMALL_RETINA:
+			return this.getPath() + ".img.620.high." + this.getExtension() + this.getSuffix();
+
+		case LARGE: case RETINA:
+			return this.getPath() + ".img.320.medium." + this.getExtension() + this.getSuffix();
+
+		default:
+			throw new UnsupportedOperationException("Image Interchange size not supported");}
 	}
-	
+
 	private int getImageWidth() {
 		String damAssetPath = this.getAsset().getPath();
 		Resource  imageMedataData = this.resourceResolver.getResource(damAssetPath+"/jcr:content/metadata");
@@ -109,7 +128,7 @@ public class DiscoverRelatedImageHelper {
 		String imageWidth = imageProperies.get("tiff:ImageWidth", "0");
 		return Integer.parseInt(imageWidth);
 	}
-	
+
 	private Asset getAsset() {
 		if (this.asset == null)
 		{
@@ -131,7 +150,7 @@ public class DiscoverRelatedImageHelper {
 	public void setPath(String path) {
 		this.path = path;
 	}
-	
+
 	public String getOriginalImagePath() {
 		return originalImagePath;
 	}
@@ -148,56 +167,46 @@ public class DiscoverRelatedImageHelper {
 		this.extension = extension;
 	}
 
-
 	public String getSuffix() {
 		return suffix;
 	}
-
 
 	public void setSuffix(String suffix) {
 		this.suffix = suffix;
 	}
 
-
 	public String getAlt() {
 		return alt;
 	}
-
 
 	public void setAlt(String alt) {
 		this.alt = alt;
 	}
 
-
 	public Boolean isActivated() {
 		return activated;
 	}
-
 
 	public void setActivated(Boolean activated) {
 		this.activated = activated;
 	}
 
-
 	public String getCaption() {
 		return caption;
 	}
-
 
 	public void setCaption(String caption) {
 		this.caption = caption;
 	}
 
-
 	public String getImageLinkURL() {
 		return imageLinkURL;
 	}
 
-
 	public void setImageLinkURL(String imageLinkURL) {
 		this.imageLinkURL = imageLinkURL;
 	}	
-	
+
 	public String getNewWindowHtml() {
 		if (this.newwindow)
 		{	
@@ -208,36 +217,12 @@ public class DiscoverRelatedImageHelper {
 			return "";
 		}
 	}
-	
-	// WR-890
-	public Boolean getAddMarginBottom() {
-		return addMarginBottom;
-	}
-	
-	// WR-890
-	public void setAddMarginBottom(Boolean addMarginBottom) {
-		this.addMarginBottom = addMarginBottom;
-	}
-	
-	private boolean hasMobileImage(String path) {
-		
-		return false;
-	}
-
 
 	public String getMobileImagePath() {
-		/*String mobilePath = this.originalImagePath.substring(0, this.originalImagePath.lastIndexOf(".")) + "_mobile." + this.extension;
-		
-		return mobilePath;*/
 		return this.mobileImagePath;
 	}
 
 	public void setMobileImagePath(String mobileImagePath) {
 		this.mobileImagePath = mobileImagePath;
 	}
-	
-	
-	
-	
-		
 }
