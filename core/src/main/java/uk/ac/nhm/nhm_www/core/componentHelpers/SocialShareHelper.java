36c66305d7bcc7eb4a0a11029ab6228492400511
Alistair Paterson <a.paterson@nhm.ac.uk>
2017-07-31 15:11:55 +0100

    WR-1087: Added helper to remove /content/nhmwww/en/home from email link

--- core/src/main/java/uk/ac/nhm/nhm_www/core/componentHelpers/SocialShareHelper.java
+++ core/src/main/java/uk/ac/nhm/nhm_www/core/componentHelpers/SocialShareHelper.java
@@ -4,9 +4,22 @@ import com.adobe.cq.sightly.WCMUse;
 
 public class SocialShareHelper extends WCMUse {
 	
+	private String emailLink;
+	
 	@Override
 	public void activate() throws Exception {
-		//TODO
+		
+		//Strip '/content/nhmwww/en/home' from page path
+		String pagePath = getResourcePage().getPath();
+		if(pagePath.contains("/content/nhmwww/en/home")) {
+			pagePath = pagePath.replaceAll("/content/nhmwww/en/home", "");
+		}
+		
+		this.emailLink = pagePath;
+	}
+
+	public String getEmailLink() {
+		return emailLink;
 	}
 
 }
