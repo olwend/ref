d43512ce5d4b809f5041b0160feea99cf55a1b0a
Alistair Paterson <a.paterson@nhm.ac.uk>
2017-08-22 11:21:04 +0100

    WR-1092: Created article helper, component, and template

---
+++ core/src/main/java/uk/ac/nhm/nhm_www/core/componentHelpers/ArticleHelper.java
@@ -0,0 +1,513 @@
+package uk.ac.nhm.nhm_www.core.componentHelpers;
+
+import java.text.DateFormatSymbols;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
+import org.joda.time.DateTime;
+import org.joda.time.MutableDateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import uk.ac.nhm.nhm_www.core.model.FileReference;
+
+import com.adobe.granite.xss.XSSAPI;
+import com.day.cq.commons.ImageResource;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.foundation.Image;
+
+/**
+ * Discover Publication Component Helper class.
+ */
+public class ArticleHelper {
+	private static final String IMAGE_HEAD_TYPE = "image";
+	private static final String VIDEO_HEAD_TYPE = "video";
+	
+	/*
+	 * Repository Attribute names.
+	 */
+	public static final String TITLE_ATTRIBUTE_NAME 		= "jcr:title";
+	public static final String INTRODUCTION_ATTRIBUTE_NAME  = "introduction";
+	public static final String SNIPPET_ATTRIBUTE_NAME		= "snippet";
+	public static final String IMAGE_CAPTION_ATTRIBUTE_NAME = "imageCaption";
+	public static final String PINNED_ATTRIBUTE_NAME 		= "pinned";
+	public static final String PINNED_DATE_ATTRIBUTE_NAME   = "pinnedDate";
+	public static final String TAGS_ATTRIBUTE_NAME 		 	= "cq:tags";
+	public static final String TYPE_ATTRIBUTE_NAME 			= "type";
+	public static final String HEAD_TYPE_ATTRIBUTE_NAME 	= "headType";
+	public static final String CREATION_DATE_ATTRIBUTE_NAME = "jcr:created";
+	public static final String IMAGE_ALT_ATTRIBUTE_NAME		= "image/alt";
+	public static final String DATE_PUBLISHED				= "datepublished";
+	public static final String DATE_LAST_UDPATED			= "datelastupdated";
+	
+	/*
+	 * SubResource Names.
+	 */
+	private static final String IMAGE_RESOURCE_NAME = "image";
+	private static final String VIDEO_RESOURCE_NAME = "video";
+	
+	/*
+	 * Component Properties.
+	 */
+	private ValueMap properties;
+	
+	/*
+	 * Component Resource.
+	 */
+	private Resource resource;
+	
+	private String imagePath;
+	private String imageNodePath;
+	private String imageExtension;
+	private String imageSuffix;
+	private String imageAlt;
+	private boolean imageConfigured;
+	
+	private String ogTitle;
+	private String ogDescription;
+	private String ogImagePath;
+	private String pageTitle;
+	private String pageDescription;
+	private String selectTab;
+	
+	private String date;
+	
+	/**
+	 * Helper Class Constructor.
+	 * @param resource {@link Resource Component Resource}.
+	 * @param request 
+	 * @param xssAPI
+	 */
+	public ArticleHelper(final Resource resource, final HttpServletRequest request, final XSSAPI xssAPI) {
+		this.resource   = resource;
+		this.properties = resource.adaptTo(ValueMap.class);
+		
+		FileReference fileReference = new FileReference.FileReferenceBuilder(properties.get("image/fileReference", ""), properties, resource, request)
+		.image(new ImageResource(resource))
+		.xssApi(xssAPI)
+		.build();
+		
+		this.imageConfigured = fileReference.getHasImage();
+		
+		if (this.imageConfigured) {
+			this.imagePath = fileReference.getPath();
+			this.imageNodePath = fileReference.getNodePath();
+			this.imageExtension = fileReference.getExtension();
+			this.imageSuffix = fileReference.getExtension();
+			this.imageAlt = fileReference.getAlt();
+		}
+		
+		String datePublished = properties.get(DATE_PUBLISHED, String.class);
+		String dateLastUpdated = properties.get(DATE_LAST_UDPATED, String.class);
+		
+		DateTimeFormatter dateFormatter = DateTimeFormat.forPattern("MM/dd/yy");
+		
+		if(dateLastUpdated != null) {
+			DateTime dt = dateFormatter.parseDateTime(dateLastUpdated);
+			MutableDateTime mdt = dt.toMutableDateTime();
+			this.date = "Last updated " + mdt.getDayOfMonth() + " " + getMonth(mdt.getMonthOfYear()) + " " + mdt.getYear();
+		}
+		else if(datePublished != null) {
+			DateTime dt = dateFormatter.parseDateTime(datePublished);
+			MutableDateTime mdt = dt.toMutableDateTime();
+			this.date = mdt.getDayOfMonth() + " " + getMonth(mdt.getMonthOfYear()) + " " + mdt.getYear();
+		}
+		else {
+			this.date = "Please set a published date in the dialog";
+		}
+	}
+	
+	/**
+	 * Helper Class Constructor.
+	 * @param resource {@link Resource Component Resource}.
+	 */
+	public ArticleHelper(Resource resource) {
+		setResource(resource);
+		setProperties(resource.adaptTo(ValueMap.class));
+		
+		init();
+	}
+	
+	private void init() {
+		//Initialise variables from Facebook tab
+		
+		//Set title		
+		String ogTitle = "";
+		if(getProperties().get("ogtitle") != null) {
+			ogTitle = getProperties().get("ogtitle", String.class);
+			
+		}
+		setOgTitle(ogTitle);
+		
+		//Set description
+		String ogDescription = "";
+		if(getProperties().get("ogdescription") != null) {
+			ogDescription = getProperties().get("ogdescription", String.class);
+		}
+		setOgDescription(ogDescription);
+
+		//Set image path - value is dependent on which radio button is selected
+		String ogImagePath = "";
+		if(getProperties().get("selectTab") != null) {
+			if(getProperties().get("selectTab").equals("radioImage")) {
+				if(getProperties().get("ogimagepath") != null) {
+					ogImagePath = getProperties().get("ogimagepath", String.class);
+				}
+			}
+			else if(getProperties().get("selectTab").equals("radioVideo")) {
+				if(getProperties().get("ogvideopath") != null) {
+					ogImagePath = getProperties().get("ogvideopath", String.class);
+				}
+			}
+		}
+		setOgImagePath(ogImagePath);
+
+		//Set title - default from 'Basic' tab
+		String pageTitle = "";
+		if(getProperties().get("jcr:title") != null) {
+			pageTitle = getProperties().get("jcr:title", String.class);
+		}
+		setPageTitle(pageTitle);
+		
+		//Set description - default from 'Basic' tab
+		String pageDescription = "";
+		if(getProperties().get("jcr:description") != null) {
+			pageDescription = getProperties().get("jcr:description", String.class);
+		}
+		setPageDescription(pageDescription);
+		
+		//Set value of radio buttons
+		String selectTab = "";
+		if(getProperties().get("selectTab") != null) {
+			selectTab = getProperties().get("selectTab", String.class);
+		}
+		setSelectTab(selectTab);
+	}
+	
+	public String getMonth(int month) {
+	    return new DateFormatSymbols().getMonths()[month-1];
+	}
+	
+	public Resource getResource() {
+		return resource;
+	}
+
+	public void setResource(Resource resource) {
+		this.resource = resource;
+	}
+	
+	/**
+	 * Checks if the component has enough configuration to show its content.
+	 * @return <code>true</code> if the component has configured at least Title, Introduction and Image or Video. <code>false</code> in otherwise.
+	 */
+	public boolean isConfigured() {
+		return this.properties !=null 
+				&& this.properties.get(TITLE_ATTRIBUTE_NAME, String.class) != null
+				&& this.properties.get(INTRODUCTION_ATTRIBUTE_NAME, String.class) != null
+				&& (this.isImageHeadType() && this.imageConfigured
+				    || this.isVideoHeadType() && this.getVideo() != null);
+	}
+	
+	/**
+	 * Gets the Title component.
+	 * @return The title configured on the component or an empty string if is not configured.
+	 */
+	public String getTitle() {
+		if (this.properties == null) {
+			return "";
+		}
+		return this.properties.get(TITLE_ATTRIBUTE_NAME, "");
+	}
+	
+	/**
+	 * Gets the Introduction component.
+	 * @return The introduction configured on the component or an empty string if is not configured.
+	 */
+	public String getIntroduction() {
+		if (this.properties == null) {
+			return "";
+		}
+		return this.properties.get(INTRODUCTION_ATTRIBUTE_NAME, "");
+	}
+	
+	/**
+	 * Checks if the component has the Snippet Text configured.
+	 * @return <code>true</code> if the component has configured Snippet Text Attribute. <code>false</code> in otherwise.
+	 */
+	public boolean hasSnippet() {
+		if (this.properties == null) {
+			return false;
+		}
+		return this.properties.get(SNIPPET_ATTRIBUTE_NAME, String.class) != null;
+	}
+
+	/**
+	 * Gets the Snippet Text configured on the component.
+	 * @return The Snippet Text configured on the component or an empty string if is not configured.
+	 */
+	public String getSnippet() {
+		if (this.properties == null) {
+			return "";
+		}
+		return this.properties.get(SNIPPET_ATTRIBUTE_NAME, "");
+	}
+	
+	/**
+	 * Checks if the component has and Image Caption configured.
+	 * @return <code>true</code> if the component has configured the Image Caption Attribute. <code>false</code> in otherwise.
+	 */
+	public boolean hasImageCaption() {
+		if (this.properties == null) {
+			return false;
+		}
+		return this.properties.get(IMAGE_CAPTION_ATTRIBUTE_NAME, String.class) != null;
+	}
+	
+	/**
+	 * Checks if the component has and Image Caption configured.
+	 * @return <code>true</code> if the component has configured the Image Caption Attribute. <code>false</code> in otherwise.
+	 */
+	public boolean hasImageAltText() {
+		if (this.properties == null) {
+			return false;
+		}
+		return this.properties.get(IMAGE_ALT_ATTRIBUTE_NAME, String.class) != null;
+	}
+	
+	/**
+	 * Gets the Image Text Caption configured on the component.
+	 * @return The Snippet Text configured on the component or an empty string if is not configured.
+	 */
+	public String getImageCaption() {
+		if (this.properties == null) {
+			return "";
+		}
+		return this.properties.get(IMAGE_CAPTION_ATTRIBUTE_NAME, "");
+	}
+	
+	/**
+	 * Gets the Image Text Caption configured on the component.
+	 * @return The Snippet Text configured on the component or an empty string if is not configured.
+	 */
+	public String getImageAltText() {
+		if (this.properties == null) {
+			return "";
+		}
+		return this.properties.get(IMAGE_ALT_ATTRIBUTE_NAME, "");
+	}
+	
+	/**
+	 * Checks if the component has been Pinned.
+	 * @return <code>true</code> if the component has the attribute Pinned set true. <code>false</code> in otherwise.
+	 */
+	public boolean isPinned() {
+		if (this.properties == null) {
+			return false;
+		}
+		return this.properties.get(PINNED_ATTRIBUTE_NAME, false);
+	}
+	
+	/**
+	 * Gets the Pinned Date configured on the component.
+	 * @return The Pinned Date configured on the component or <code>null</code> if is not configured.
+	 */
+	public Date getPinnedDate() {
+		if (!this.isPinned()) {
+			return null;
+		}
+		
+		return properties.get(PINNED_DATE_ATTRIBUTE_NAME, Calendar.class).getTime();
+	}
+	
+	/**
+	 * Gets the tags configured on the component.
+	 * @return The tags configured.
+	 */
+	public String[] getTags() {
+		if (this.properties == null) {
+			return null;
+		}
+		return this.properties.get(TAGS_ATTRIBUTE_NAME, String[].class);
+	}
+	
+	/**
+	 * Gets the Head Type configured on the component. This can be image or video.
+	 * @return The Head Type (Image or Video) configured on the component or &quot;image%quot; if is not configured.
+	 */
+	public String getHeadType() {
+		if (this.properties == null) {
+			return IMAGE_HEAD_TYPE;
+		}
+		return this.properties.get(HEAD_TYPE_ATTRIBUTE_NAME, IMAGE_HEAD_TYPE);
+	}
+
+	/**
+	 * Checks if the component has configured the Head Type as Image.
+	 * @return <code>true</code> if the Head Type is &quot;image&quot;.
+	 */
+	public boolean isImageHeadType() {
+		return IMAGE_HEAD_TYPE.equals(this.getHeadType());
+	}
+	
+	/**
+	 * Checks if the component has configured the Head Type as Video.
+	 * @return <code>true</code> if the Head Type is &quot;video&quot;.
+	 */
+	public boolean isVideoHeadType() {
+		return VIDEO_HEAD_TYPE.equals(this.getHeadType());
+	}
+	
+	/**
+	 * Gets the Image configure as Head, if the Head Type is configured as Image if not will return <code>null</code>.
+	 * @return The {@link Image Image object} configured on the component if is configured the Head Type as Image or <code>null</code> otherwise. 
+	 */
+	/*public Image getImage() {
+		if (!this.isImageHeadType()) {
+			return null;
+		}
+		
+		final Resource imageResource = this.resource.getChild(IMAGE_RESOURCE_NAME);
+		if (imageResource == null) {
+			return null;
+		}
+		
+		final DiscoverHeadImage image = new DiscoverHeadImage(imageResource);
+		return image.getImage();
+	}*/
+	
+	/**
+	 * Gets the YouTube Video Id configure as Head, if the Head Type is configured as Video if not will return <code>null</code>.
+	 * @return The Youtube Video Id configured on the component if is configured the Head Type as Video or <code>null</code> otherwise. 
+	 */
+	public String getVideo() {
+		if (!this.isVideoHeadType()) {
+			return null;
+		}
+		
+		final Resource videoResource = this.resource.getChild(VIDEO_RESOURCE_NAME);
+		if (videoResource == null) {
+			return null;
+		}
+		
+		final DiscoverHeadVideo video = new DiscoverHeadVideo(videoResource);
+		return video.getYouTubeVideoId();
+	}
+	
+	/**
+	 * Gets the Publication Type configured on the component.
+	 * @return The Publication Type configured on the component.
+	 */
+	public String getType() {
+		if (this.properties == null) {
+			return null;
+		}
+		return this.properties.get(TYPE_ATTRIBUTE_NAME, String.class);
+	}
+	
+	/**
+	 * Gets the Creation Date configured on the component.
+	 * @return The Creation Date of the component.
+	 */
+	public Date getCreationDate() {
+		final Calendar calendar = this.properties.get(CREATION_DATE_ATTRIBUTE_NAME, Calendar.class);
+		
+		if (calendar == null) {
+			return null;
+		}
+		
+		return calendar.getTime();
+	}
+	
+	public String getImagePath() {
+		return this.imagePath;
+	}
+
+	public String getImageNodePath() {
+		return this.imageNodePath;
+	}
+	
+	public String getImageExtension() {
+		return this.imageExtension;
+	}
+
+	public String getImageSuffix() {
+		return this.imageSuffix;
+	}
+
+	public String getImageAlt() {
+		return this.imageAlt;
+	}
+	
+	public boolean isImageConfigured() {
+		return this.imageConfigured;
+	}
+	
+	public ValueMap getProperties() {
+		return properties;
+	}
+
+	public void setProperties(ValueMap properties) {
+		this.properties = properties;
+	}
+
+	public String getOgTitle() {
+		return ogTitle;
+	}
+
+	public void setOgTitle(String ogTitle) {
+		this.ogTitle = ogTitle;
+	}
+
+	public String getOgDescription() {
+		return ogDescription;
+	}
+
+	public void setOgDescription(String ogDescription) {
+		this.ogDescription = ogDescription;
+	}
+
+	public String getOgImagePath() {
+		return ogImagePath;
+	}
+
+	public void setOgImagePath(String ogImagePath) {
+		this.ogImagePath = ogImagePath;
+	}
+
+	public String getPageTitle() {
+		return pageTitle;
+	}
+
+	public void setPageTitle(String pageTitle) {
+		this.pageTitle = pageTitle;
+	}
+
+	public String getPageDescription() {
+		return pageDescription;
+	}
+
+	public void setPageDescription(String pageDescription) {
+		this.pageDescription = pageDescription;
+	}
+
+	public String getSelectTab() {
+		return selectTab;
+	}
+
+	public void setSelectTab(String selectTab) {
+		this.selectTab = selectTab;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+}
---
+++ ui.apps/src/main/content/jcr_root/apps/nhmwww/components/functional/article/article.jsp
@@ -0,0 +1,100 @@
+<%@include file="/apps/nhmwww/components/global.jsp" %>
+<%@page session="false" 
+		import="com.day.cq.wcm.api.WCMMode,
+				java.util.Calendar,
+				uk.ac.nhm.nhm_www.core.componentHelpers.ArticleHelper"%>
+
+<%
+	final ArticleHelper helper = new ArticleHelper(resource, request, xssAPI);
+
+	if (!helper.isConfigured()) {
+%>
+	<div class="row">
+		<span class="noconfigured">Please configure the article page correctly.</span>
+	</div>
+<%
+		return;
+	}
+
+	if (helper.isImageHeadType()) {
+		if (helper.isImageConfigured()) {
+%>
+	<div class="row">
+		<div class="discover-image small-12 medium-12 large-12 columns">
+<%
+  			WCMMode beforeMode = WCMMode.fromRequest(slingRequest);
+  			WCMMode.PREVIEW.toRequest(slingRequest);
+%>
+	<cq:include path="image" resourceType="nhmwww/components/functional/foundation5image" />
+
+<%
+			beforeMode.toRequest(slingRequest);
+			if (helper.hasImageCaption()) {
+%>
+			<div class="discover-image-caption-icon small-1 medium-1 large-1">
+				<a>
+					<i class="ico svg-ico" data-svg-src="/etc/designs/nhmwww/img/svg-icons/icon_l_general_info.svg" data-svg-title="icon__general_info" data-stroke-width="4" data-base-color="#FFFFFF"></i>
+				</a>
+			</div>
+			<div class="discover-image-caption" style="display: none;">
+				<div class="close-icon">
+					<a>
+						<i class="ico svg-ico" data-svg-src="/etc/designs/nhmwww/img/svg-icons/icon_l_general_close.svg" data-svg-title="icon__general_close" data-stroke-width="4" data-base-color="#FFFFFF"></i>
+					</a>
+				</div>
+				<%= xssAPI.filterHTML(helper.getImageCaption()) %>
+			</div>
+<%
+			}
+%>
+		</div>
+	</div>
+<%
+		}
+	}
+	
+	if (helper.isVideoHeadType()) {
+		final String videoId = helper.getVideo();
+%>
+	<div class="row">
+		<div class="discover-video small-12 medium-12 large-12 columns">
+			<div class="video-wrapper" data-nhm-videoid="<%= videoId %>">
+				<div id="<%= videoId %>" class="youtubeplayer"></div>
+			</div>
+		</div>
+	</div>
+<% 
+	}
+%> 
+	<div class="section row2cells21">
+		<div class="row">
+			<div class="small-12 medium-8 large-8 columns">
+				<h1><%= xssAPI.filterHTML(helper.getTitle()) %></h1>
+				<div class="discover-publication-date"><%=helper.getDate() %></div>
+				<div class="introduction"><%= xssAPI.filterHTML(helper.getIntroduction()) %></div>
+				<cq:include path="par" resourceType="foundation/components/parsys"/>
+				<div class="discover-publication-footer">
+					<div class="flags small-12 medium-12 large-12 columns">
+						<div class="facebook small-12 medium-3 large-3 columns">
+							<cq:includeClientLib categories="cq.social.plugins.facebook"/>
+							<div id="fb-root"></div>
+							<script src="//connect.facebook.net/en_US/all.js"></script>
+		
+							<script type="text/javascript">
+			    				$(document).ready(function(){
+				        			FB.Event.subscribe('xfbml.render',function(){
+				            			$('.FB_Loader').css('background','url()');
+				        			});
+			    				});
+							</script>
+							<div class="fb-like" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
+						</div>
+						<div class="rest small-12 medium-9 large-9 columns"></div>
+					</div>
+				</div>
+			</div>
+			<div class="small-12 medium-4 large-4 columns">
+				<cq:include path="ctapar" resourceType="foundation/components/parsys" />
+			</div>
+		</div>
+	</div>
