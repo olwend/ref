be4560226a2513b838efb4a22ba63801dc4e39eb
Alistair Paterson <a.paterson@nhm.ac.uk>
2017-11-29 14:01:36 +0000

    WR-1085: Added dialogs for tagged content and press release pages

--- core/src/main/java/uk/ac/nhm/core/componentHelpers/ListHelper.java
+++ core/src/main/java/uk/ac/nhm/core/componentHelpers/ListHelper.java
@@ -30,6 +30,7 @@ public class ListHelper extends HelperBase {
 	protected Boolean newwindow;
 	protected Boolean initialised;
 	
+	
 	public ListHelper(ValueMap properties, PageManager pageManager, Page currentPage, HttpServletRequest request, ResourceResolver resourceResolver) {
 		this.properties = properties;
 		this.pageManager = pageManager;
@@ -39,13 +40,13 @@ public class ListHelper extends HelperBase {
 		init();
 	}
 
+
 	protected void init() {
 		Page landingPage = currentPage.getAbsoluteParent(4);
 		
 		if (this.properties.get("title", String.class) != null) {
 		    this.componentTitle = this.properties.get("title",String.class);
 		}
-		
 		if (this.properties.get("hyperlink", String.class) != null) {
 		    this.hyperLink = LinkUtils.getFormattedLink(this.properties.get("hyperlink",String.class));
 		}
@@ -54,8 +55,10 @@ public class ListHelper extends HelperBase {
 			Iterator<Page> children = landingPage.listChildren(new PageFilter(request));
 			processChildren(children);
 		}
+		
 	}
-	
+
+
 	protected void processChildren(Iterator<Page> children) {
 		listElements = new ArrayList<Object>();
 		List<ListElementImpl> elements = new ArrayList<ListElementImpl>();
@@ -66,53 +69,71 @@ public class ListHelper extends HelperBase {
 				elements.add(element);
 			}
 		}
+		
 	}
+
 
 	public Boolean isInitialised() {
 		return initialised;
 	}
 
+
 	public void setInitialised(Boolean initialised) {
 		this.initialised = initialised;
 	}
+
 
 	public String getComponentTitle() {
 		return componentTitle;
 	}
 
+
 	public void setComponentTitle(String componentTitle) {
 		this.componentTitle = componentTitle;
 	}
+
 
 	public String getHyperLink() {
 		return hyperLink;
 	}
 
+
 	public void setHyperLink(String hyperLink) {
 		this.hyperLink = hyperLink;
 	}
+	
 	
     public Boolean getNewwindow() {
 		return newwindow;
 	}
 
+
 	public void setNewwindow(Boolean newwindow) {
 		this.newwindow = newwindow;
 	}
+
 
 	public boolean validateHyperlink() {
     	return LinkUtils.validateUrl(this.hyperLink);
     }
     
+	
     public List<Object> getChildrenElements() {
     	return  this.listElements;
     }
     
+    
     public void addListElement(final Object element) {
     	this.listElements.add(element);
+
     }
     
     public void addAllListElements(final List<Object> elements) {
     	this.listElements.addAll(elements);
     }
+
+
+
+	
+	
 }
