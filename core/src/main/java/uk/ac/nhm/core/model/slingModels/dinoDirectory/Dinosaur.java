25ff0999953229ca170661d661090302d34a0221
Alistair Paterson <a.paterson@nhm.ac.uk>
2018-01-05 16:18:26 +0000

    dd-test: Renamed dinoDirectory component to Dinosaur component Changed in Dinosaur component to pull single dinosaur with name from dialog Added dynamic drop down to dialog that pulls list genera from API Added dino directory component group

---
+++ core/src/main/java/uk/ac/nhm/core/model/slingModels/dinoDirectory/Dinosaur.java
@@ -0,0 +1,81 @@
+package uk.ac.nhm.core.model.slingModels.dinoDirectory;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Model(adaptables = Resource.class)
+public class Dinosaur {
+
+	private final static Logger LOG = LoggerFactory.getLogger(Dinosaur.class);
+	
+	@Inject
+	private String name;
+	
+	private String genus;
+	private String diet;
+	private String imageUrl;
+	
+	private static final String BASE_URL = "http://staging.nhm.ac.uk/api/dino-directory-api/dinosaur/name";
+	
+	@PostConstruct
+	protected void init() {
+		String requestUrl = BASE_URL + "/" + name;
+		
+		HttpClient httpClient = new HttpClient();
+		GetMethod getMethod = new GetMethod(requestUrl);
+		
+		LOG.error(requestUrl);
+		
+		try {
+			httpClient.executeMethod(getMethod);
+			JSONObject dinosaur = new JSONObject(getMethod.getResponseBodyAsString());
+			JSONArray dinosaurMediaArray = dinosaur.getJSONArray("mediaCollection");
+			JSONObject dinosaurMedia = dinosaurMediaArray.getJSONObject(0);
+			
+			this.setGenus(dinosaur.getString("genus"));
+			this.setDiet(dinosaur.getString("diet"));
+			
+			this.setImageUrl("http://www.nhm.ac.uk/resources/nature-online/life/dinosaurs/dinosaur-directory"
+					+ dinosaurMedia.getString("mediaTypePath")
+					+ dinosaurMedia.getString("mediaContentTypePath")
+					+ "/small/" + dinosaurMedia.getString("identifier") + ".jpg");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String getGenus() {
+		return genus;
+	}
+
+	public void setGenus(String genus) {
+		this.genus = genus;
+	}
+
+	public String getDiet() {
+		return diet;
+	}
+
+	public void setDiet(String diet) {
+		this.diet = diet;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	
+}
