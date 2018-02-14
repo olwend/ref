2d6d2221784e00a55e9aaa497026d42f5abee288
Alistair Paterson <a.paterson@nhm.ac.uk>
2018-02-14 13:45:09 +0000

    WR-1213: Added service to obain environment for Dino Directory API calls

--- core/src/main/java/uk/ac/nhm/core/model/slingModels/dinoDirectory/Dinosaur.java
+++ core/src/main/java/uk/ac/nhm/core/model/slingModels/dinoDirectory/Dinosaur.java
@@ -12,15 +12,22 @@ import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Source;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import uk.ac.nhm.core.services.DinoDirectoryEnvironmentService;
+
 @Model(adaptables = Resource.class)
 public class Dinosaur {
 
 	private final static Logger LOG = LoggerFactory.getLogger(Dinosaur.class);
+	
+	@Inject
+	@Source("osgi-services")
+	DinoDirectoryEnvironmentService service;
 	
 	@Inject
 	private String name;
@@ -40,11 +47,11 @@ public class Dinosaur {
 	
 	private String imageUrl;
 	
-	private static final String BASE_URL = "http://staging.nhm.ac.uk/api/dino-directory-api/dinosaur/name";
-	
 	@PostConstruct
 	protected void init() {
-		String requestUrl = BASE_URL + "/" + name;
+		final String BASE_URL = service.getDinoDirectoryUrl();
+		
+		String requestUrl = BASE_URL + "/dinosaur/name/" + name;
 		
 		HttpClient httpClient = new HttpClient();
 		GetMethod getMethod = new GetMethod(requestUrl);
--- ui.apps/src/main/content/jcr_root/apps/nhmwww/components/functional/dinodirectory/dinosaur/datasource/datasource.jsp
+++ ui.apps/src/main/content/jcr_root/apps/nhmwww/components/functional/dinodirectory/dinosaur/datasource/datasource.jsp
@@ -22,7 +22,7 @@
 <%@taglib prefix="cq" uri="http://www.day.com/taglibs/cq/1.0"%>
 <cq:defineObjects />
 <%
-	String requestUrl = "http://staging.nhm.ac.uk/api/dino-directory-api/dinosaur/?view=genus";
+	String requestUrl = "http://www.nhm.ac.uk/api/dino-directory-api/dinosaur/?view=genus";
 
 	HttpClient httpClient = new HttpClient();
 	GetMethod getMethod = new GetMethod(requestUrl);
