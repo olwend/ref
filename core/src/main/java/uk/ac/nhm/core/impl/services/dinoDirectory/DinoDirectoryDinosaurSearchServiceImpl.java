73e2d8ca254bffcd07265a621cc8bc35f9096bb3
James Stone <james.stone@nhm.ac.uk>
2018-03-19 16:52:20 +0000

    WR-1214: Style and backend update

--- core/src/main/java/uk/ac/nhm/core/impl/services/dinoDirectory/DinoDirectoryDinosaurSearchServiceImpl.java
+++ core/src/main/java/uk/ac/nhm/core/impl/services/dinoDirectory/DinoDirectoryDinosaurSearchServiceImpl.java
@@ -24,42 +24,42 @@ public class DinoDirectoryDinosaurSearchServiceImpl implements DinoDirectoryDino
 	private final static Logger LOG = LoggerFactory.getLogger(DinoDirectoryDinosaurSearchServiceImpl.class);
 
 	private final static String BASE_CONTENT_URL = "/content/nhmwww/en/home/discover/dino-directory/";
-	
+
 	private HttpClient httpClient = new HttpClient();
-	
+
 	public List<Map<String, String>> getDinosaurList(String filterOne, String filterTwo, String environmentUrl) {
 		List<Map<String, String>> dinosaurList = new ArrayList<Map<String, String>>();
-		
+
 		final String BASE_URL = environmentUrl;
-		
-		String requestUrl = BASE_URL + "/dinosaur/galleryview/" + filterOne + "/" + filterTwo;
-		
+
+		String requestUrl = BASE_URL + "/dinosaur/" + filterOne + "/" + filterTwo;
+
 		GetMethod getMethod = new GetMethod(requestUrl);
-		
+
 		LOG.info(requestUrl);
-		
+
 		try {
 			httpClient.executeMethod(getMethod);
 			JSONArray dinosaurs = new JSONArray(getMethod.getResponseBodyAsString());
 			for(int i=0; i<dinosaurs.length(); i++) {
 				Map<String, String> dinosaurMap = new HashMap<String, String>();
-				
+
 				dinosaurMap.put("genus", dinosaurs.getJSONObject(i).getString("genus"));
 				dinosaurMap.put("url", BASE_CONTENT_URL + dinosaurs.getJSONObject(i).getString("genus").toLowerCase() + ".html");
-				
+
 				JSONObject dinosaurMedia = dinosaurs.getJSONObject(i).getJSONArray("mediaCollection").getJSONObject(0);
-	
+
 				String imageUrl = "http://www.nhm.ac.uk/resources/nature-online/life/dinosaurs/dinosaur-directory/images/reconstruction/thumb/"
 						+ dinosaurMedia.getString("identifier") + ".gif";
-				
+
 				dinosaurMap.put("imageUrl", imageUrl);
-				
+
 				dinosaurList.add(dinosaurMap);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return dinosaurList;
 	}
 
@@ -67,7 +67,7 @@ public class DinoDirectoryDinosaurSearchServiceImpl implements DinoDirectoryDino
 	public String getTitle(String filterOne, String filterTwo) {
 		//Filter two value has been modified to create a string that works for API calls.
 		//Need to turn it back into presentable string.
-		
+
 		String title = filterTwo.replaceAll("%20", " ");
 		title = title.replaceAll("-", " ");
 
@@ -75,43 +75,43 @@ public class DinoDirectoryDinosaurSearchServiceImpl implements DinoDirectoryDino
 			title = WordUtils.capitalizeFully(title);
 			title = title + " dinosaurs";
 		}
-		
+
 		if(filterOne.equals("country")) {
 			title = WordUtils.capitalizeFully(title);
 			title = "Dinosaurs in " + title;
 		}
-		
+
 		if(filterOne.equals("initial")) {
 			title = "Dinosaurs beginning with " + title.toUpperCase();
 		}
-		
+
 		if(filterOne.equals("period")) {
 			title = WordUtils.capitalizeFully(title);
 			title = title.substring(0, 1).toLowerCase() + title.substring(1);
 			title = "Dinosaurs in the " + title;
 		}
-		
+
 		return title;
 	}
 
 	@Override
 	public String getDescription(String filterOne, String filterTwo, String environmentUrl) {
-		
+
 		final String BASE_URL = environmentUrl;
-		
+
 		String description = null;
 		String requestUrl = BASE_URL + "/" + filterOne + "/";
 		filterTwo = filterTwo.replaceAll("%20", " ").replaceAll("-", "").replaceAll(" ", "").toLowerCase();
-		
+
 		GetMethod getMethod = new GetMethod(requestUrl);
-		
+
 		LOG.info(requestUrl);
-		
+
 		try {
 			httpClient.executeMethod(getMethod);
 			JSONArray filterItems = new JSONArray(getMethod.getResponseBodyAsString());
 			for(int i=0; i<filterItems.length(); i++) {
-				
+
 				JSONObject filterItem = filterItems.getJSONObject(i);
 
 				if(filterOne.equals("bodyshape")) {
@@ -120,22 +120,22 @@ public class DinoDirectoryDinosaurSearchServiceImpl implements DinoDirectoryDino
 						description = filterItem.getString("description");
 					}
 				}
-				
+
 				if(filterOne.equals("country")) {
 					String filterItemTitle = filterItem.getString("country").replaceAll("-", "").replaceAll(" ", "").toLowerCase();
 					if(filterItemTitle.equals(filterTwo)) {
 						if(filterItem.getInt("dinosaurCount") > 1) {
-							description = String.valueOf(filterItem.getInt("dinosaurCount")) + " dinosaurs found in " + filterItem.getString("country");							
+							description = String.valueOf(filterItem.getInt("dinosaurCount")) + " dinosaurs found in " + filterItem.getString("country");
 						} else {
 							description = String.valueOf(filterItem.getInt("dinosaurCount")) + " dinosaur found in " + filterItem.getString("country");
 						}
 					}
 				}
-				
+
 				if(filterOne.equals("period")) {
-					
+
 				}
-				
+
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -143,12 +143,12 @@ public class DinoDirectoryDinosaurSearchServiceImpl implements DinoDirectoryDino
 
 		//Transform description
 		description = description.substring(0, 1).toUpperCase() + description.substring(1);
-		
+
 		if(filterOne.equals("bodyshape")) {
 			description = description + ".";
 		}
-		
+
 		return description;
 	}
-	
+
 }
