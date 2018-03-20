060bb65a0d1420701934cf0f726bfd9740a5303b
Dhia Shakiry <dhis@nhm.ac.uk>
2018-03-20 13:15:38 +0000

    updated from standalone build

--- ui.apps/src/main/content/jcr_root/etc/clientlibs/nhmwww/singlepurpose/ld-interactive-splash-v2/js/main2.js
+++ ui.apps/src/main/content/jcr_root/etc/clientlibs/nhmwww/singlepurpose/ld-interactive-splash-v2/js/main2.js
@@ -1,67 +1,85 @@
 
-    $interactive = $('#bigsplash-interactive #filter');
-    $title = $('.bigsplash-text--title');
-    $wrap = $('.wrap');
-    hide = false;
+$interactive = $('#bigsplash-interactive #filter');
+$title = $('.bigsplash-text--title');
+$wrap = $('.wrap');
+glow = false;
 
-    $(".animal").mouseenter(function() {
-        var audio = $('#' + this.id + '-sound')[0];
-        if(audio) {
-            audio.play();
+window.addEventListener('load', function() {
+    $('#bigsplash-interactive').addClass('animate');
+}, true);
+
+
+$(".animal").mouseenter(function() {
+    var audio = $('#' + this.id + '-sound')[0];
+    if(audio) {
+        audio.play();
+    }
+    $('#' + this.id + '-glowballs').fadeOut(700);
+});
+
+$(".animal").mouseleave(function() {
+    var audio = $('#' + this.id + '-sound')[0];
+    if(audio) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+    $('#' + this.id + '-glowballs').fadeIn(1700);
+});
+
+
+
+function torch() {
+    if(!glow) {
+
+        glow = true;
+        //$interactive.on('mousemove', update);
+        //$interactive.on('touchmove', update);
+        //$interactive.on('mouseout', removeTorch);
+        //$interactive.on('mouseleave', removeTorch);
+        $interactive.toggleClass('glow');
+        $wrap.toggleClass('glow');
+        $('.animal').toggleClass('glow');
+        $('#bigsplash-interactive').toggleClass('showLayers');
+
+        if (!/Mobi/.test(navigator.userAgent)) {
+            // not mobile!
+            $title.toggleClass('glow');
         }
-        $('#' + this.id + '-glowballs').toggleClass('glow');
-    });
+    }
 
-    $(".animal").mouseleave(function() {
-        var audio = $('#' + this.id + '-sound')[0];
-        if(audio) {
-            audio.pause();
-            audio.currentTime = 0;
+    else {
+        glow =false;
+        //$interactive.off('mousemove', update);
+        //$interactive.off('touchmove', update);
+        //$interactive.off('mouseout', removeTorch);
+        //$interactive.off('mouseleave', removeTorch);
+        $interactive.toggleClass('glow');
+        $wrap.toggleClass('glow');
+        $('.animal').toggleClass('glow');
+        $('#bigsplash-interactive').toggleClass('showLayers');
+
+        if (!/Mobi/.test(navigator.userAgent)) {
+            // not mobile!
+            $title.toggleClass('glow');
         }
+    }
+}
 
-        $('#' + this.id + '-glowballs').toggleClass('glow');
-    });
 
-
+/* Torch effect
 function update(e){
     var x = e.clientX || e.touches[0].clientX;
     var y = e.clientY || e.touches[0].clientY;
-   // $interactive.css('--cursorX', x + 'px');  jquery 1.11 (website) doesnt understand these CSS rules.
-   // $interactive.css('--cursorY', y + 'px');
+    // $interactive.css('--cursorX', x + 'px');  jquery 1.11 (website) doesnt understand these CSS rules.
+    // $interactive.css('--cursorY', y + 'px');
     document.getElementById('bigsplash-interactive').style.setProperty('--cursorX', x + 'px');
     document.getElementById('bigsplash-interactive').style.setProperty('--cursorY', y + 'px');
    }
 
-
-function torch() {
-     if(!hide) {
-         hide = true;
-         $interactive.on('mousemove', update);
-         $interactive.on('touchmove', update);
-         //$interactive.on('mouseout', removeTorch);
-         //$interactive.on('mouseleave', removeTorch);
-         $interactive.toggleClass('glow');
-         $title.toggleClass('glow');
-         $wrap.toggleClass('glow');
-         $('.animal').toggleClass('glow');
-     }
-
-    else {
-         hide=false;
-         $interactive.off('mousemove', update);
-         $interactive.off('touchmove', update);
-         //$interactive.off('mouseout', removeTorch);
-         //$interactive.off('mouseleave', removeTorch);
-         $interactive.toggleClass('glow');
-         $title.toggleClass('glow');
-         $wrap.toggleClass('glow');
-         $('.animal').toggleClass('glow');
-     }
-}
-
-
 function removeTorch() {
     $interactive.addClass('hideOut');
   }
+  */
+
 
 
