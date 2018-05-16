6c0c7eb241debe7869f63d66b243fbf92661094f
Alistair Paterson <a.paterson@nhm.ac.uk>
2018-05-16 10:36:49 +0100

    WR-1359: Added clientlib to save multifield data in node store Added to sling model to pull data from repo and expose as list Added to view to declare parsys for text/image components

--- core/src/main/java/uk/ac/nhm/core/model/slingModels/imageGallery/ImageGallery.java
+++ core/src/main/java/uk/ac/nhm/core/model/slingModels/imageGallery/ImageGallery.java
@@ -1,12 +1,21 @@
 package uk.ac.nhm.core.model.slingModels.imageGallery;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.jcr.Property;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.Model;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,10 +31,29 @@ public class ImageGallery {
 	ResourceResolver resourceResolver;
 	
 	private String components = null;
+	private List<Map<String, String>> imagePageItems = null;
 	
 	@PostConstruct
-	protected void init() {
+	protected void init() throws JSONException {
+		List<Map<String, String>> itemsList = new ArrayList<Map<String, String>>();
+		
 		this.setComponents(properties.get("carouselItems", String.class));
+
+		String[] carouselItems = properties.get("carouselItems", String[].class);
+		String carouselItemsString = "[";
+		
+		for(int i=0; i<carouselItems.length; i++) {
+			Map<String, String> imagePageItemMap = new HashMap<String, String>();
+			
+			JSONObject jsonObject = new JSONObject(carouselItems[i]);
+			imagePageItemMap.put("type", "nhmwww/components/functional/imagepage/" + jsonObject.getString("components"));
+			imagePageItemMap.put("value", jsonObject.getString("itemHeading"));
+			String par = "carouselItems/par" + (i + 1);
+			imagePageItemMap.put("par", par);
+			itemsList.add(imagePageItemMap);
+		}
+		
+		this.setImagePageItems(itemsList);
 	}
 
 	public String getComponents() {
@@ -34,6 +62,14 @@ public class ImageGallery {
 
 	public void setComponents(String components) {
 		this.components = components;
+	}
+
+	public List<Map<String, String>> getImagePageItems() {
+		return imagePageItems;
+	}
+
+	public void setImagePageItems(List<Map<String, String>> imagePageItems) {
+		this.imagePageItems = imagePageItems;
 	}
 
 }
