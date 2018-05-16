db46ae79c406e92947e2ce461f1a306ec9dccf1e
Alistair Paterson <a.paterson@nhm.ac.uk>
2018-05-16 13:43:31 +0100

    WR-1359: Changed sling model to read from nodes rather than JSON

--- core/src/main/java/uk/ac/nhm/core/model/slingModels/imageGallery/ImageGallery.java
+++ core/src/main/java/uk/ac/nhm/core/model/slingModels/imageGallery/ImageGallery.java
@@ -7,7 +7,11 @@ import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.PathNotFoundException;
 import javax.jcr.Property;
+import javax.jcr.RepositoryException;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.ResourceResolver;
@@ -30,28 +34,48 @@ public class ImageGallery {
 	@Inject
 	ResourceResolver resourceResolver;
 	
+	@Inject
+	SlingHttpServletRequest request;
+	
 	private String components = null;
 	private List<Map<String, String>> imagePageItems = null;
 	
 	@PostConstruct
-	protected void init() throws JSONException {
+	protected void init() throws JSONException, PathNotFoundException, RepositoryException {
 		List<Map<String, String>> itemsList = new ArrayList<Map<String, String>>();
 		
-		this.setComponents(properties.get("carouselItems", String.class));
-
-		String[] carouselItems = properties.get("carouselItems", String[].class);
-		String carouselItemsString = "[";
+		String imagePathItemsPath = request.getResource().getPath() + "/imagePageItems";
+		Node itemsNode = resourceResolver.getResource(imagePathItemsPath).adaptTo(Node.class);
+		NodeIterator itemsNodeIterator = itemsNode.getNodes();
 		
-		for(int i=0; i<carouselItems.length; i++) {
+		int i = 0;
+		
+		while(itemsNodeIterator.hasNext()) {
+			Node childNode = itemsNodeIterator.nextNode();
 			Map<String, String> imagePageItemMap = new HashMap<String, String>();
 			
-			JSONObject jsonObject = new JSONObject(carouselItems[i]);
-			imagePageItemMap.put("type", "nhmwww/components/functional/imagepage/" + jsonObject.getString("components"));
-			imagePageItemMap.put("value", jsonObject.getString("itemHeading"));
-			String par = "carouselItems/par" + (i + 1);
+			imagePageItemMap.put("type", "nhmwww/components/functional/imagepage/" + childNode.getProperty("components").getString());
+			imagePageItemMap.put("value", childNode.getProperty("itemHeading").getString());
+			String par = "imagePageItems/par" + (i + 1);
 			imagePageItemMap.put("par", par);
 			itemsList.add(imagePageItemMap);
+			i++;
 		}
+//		this.setComponents(properties.get("imagePageItems", String.class));
+//
+//		String[] imagePageItems = properties.get("imagePageItems", String[].class);
+//		String imagePageItemsString = "[";
+//		
+//		for(int i=0; i<imagePageItems.length; i++) {
+//			Map<String, String> imagePageItemMap = new HashMap<String, String>();
+//			
+//			JSONObject jsonObject = new JSONObject(imagePageItems[i]);
+//			imagePageItemMap.put("type", "nhmwww/components/functional/imagepage/" + jsonObject.getString("components"));
+//			imagePageItemMap.put("value", jsonObject.getString("itemHeading"));
+//			String par = "imagePageItems/par" + (i + 1);
+//			imagePageItemMap.put("par", par);
+//			itemsList.add(imagePageItemMap);
+//		}
 		
 		this.setImagePageItems(itemsList);
 	}
