9a2c2c2671f1bc34690b78c401e82a693d847053
Olwen <o.davies@nhm.ac.uk>
2018-08-21 11:32:14 +0100

    WR-1424 clean up remaining CASE config

--- core/src/main/java/uk/ac/nhm/core/componentHelpers/CTAButtonHelper.java
+++ core/src/main/java/uk/ac/nhm/core/componentHelpers/CTAButtonHelper.java
@@ -327,7 +327,8 @@ public class CTAButtonHelper extends HelperBase {
 	            case SUPPORTUS:  return "#7F206E";
 	            case SHOP:  return "#808080";
 	            case SCHOOLS:  return "#0983BB";
-	            case OURSCIENCE:  return "#357900";
+	            case OURSCIENCE:  return "#2C6100";
+//	            case OURSCIENCE:  return "#357900";
 	            case ABOUTUS:  return "#0B5C7B";
 	            case NEWSPRESS:  return "#107B5E";
 	            case WORKINGWITHUS:  return "#956624";
